9c94fc1324e64820719ed9fc17580f10c50aaf55
Aniket Gupta <gupta532@purdue.edu>
2020-05-17 20:24:01 +0530

    HTML part partially complete. Plan to use Boostrap to beautify data

--- src/main/java/com/example/coronavirustracker/models/LocationsStats.java
+++ src/main/java/com/example/coronavirustracker/models/LocationsStats.java
@@ -1,4 +1,40 @@
 package com.example.coronavirustracker.models;
 
 public class LocationsStats {
+    private String state;
+    private String country;
+    private int latestTotalCases;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestTotalCases() {
+        return latestTotalCases;
+    }
+
+    public void setLatestTotalCases(int latestTotalCases) {
+        this.latestTotalCases = latestTotalCases;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationsStats{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestTotalCases=" + latestTotalCases +
+                '}';
+    }
 }
--- src/main/java/com/example/coronavirustracker/services/CoronavirusDataService.java
+++ src/main/java/com/example/coronavirustracker/services/CoronavirusDataService.java
@@ -1,5 +1,6 @@
 package com.example.coronavirustracker.services;
 
+import com.example.coronavirustracker.models.LocationsStats;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,15 +13,24 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 
 @Service
 public class CoronavirusDataService {
 
     private static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
+    private ArrayList<LocationsStats> allStats = new ArrayList<>();
+
+
+    public ArrayList<LocationsStats> getAllStats() {
+        return allStats;
+    }
+
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException, InterruptedException {
+        ArrayList<LocationsStats> newStats = new ArrayList<>();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(DATA_URL))
@@ -30,12 +40,16 @@ public class CoronavirusDataService {
 
         StringReader csvReader = new StringReader(httpResponse.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvReader);
+
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-           // System.out.println(state);
-            String country = record.get("Country/Region");
-            System.out.println(country);
+            LocationsStats locationsStat = new LocationsStats();
+            locationsStat.setState(record.get("Province/State"));
+            locationsStat.setCountry(record.get("Country/Region"));
+            locationsStat.setLatestTotalCases(Integer.parseInt(record.get(record.size() - 1)));
+            newStats.add(locationsStat);
         }
+
+        this.allStats = newStats;
 
     }
 }
